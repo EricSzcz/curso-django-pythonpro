c8aa52c6cce1003cab40027319fc4203b2eb5c74
Ericszcz <e.ericszcz@gmail.com>
2020-03-07 20:20:26 -0300

    Integração de videos com Vimeo
    close #87

---
+++ pypro/aperitivos/tests.py
@@ -0,0 +1,23 @@
+import pytest
+from django.urls import reverse
+
+from pypro.django_assertions import assert_contains
+
+
+@pytest.fixture
+def resp(client):
+    return client.get(reverse('aperitivos:video', args=('motivacao',)))
+
+
+def test_status_code(resp):
+    assert resp.status_code == 200
+
+
+def test_title_video(resp):
+    assert_contains(resp, '<h1>Video aperitivo: Motivação</h1>')
+
+
+def test_conteudo_video(resp):
+    assert_contains(resp, '<iframe src="https://player.vimeo.com/video/396196904"')
+
+
