951a5f38123a7aa18f26372f315a8fd64d6dd1d7
Ericszcz <e.ericszcz@gmail.com>
2020-03-08 15:56:23 -0300

    http e templates

--- pypro/base/tests/test_home.py
+++ pypro/base/tests/test_home.py
@@ -15,7 +15,7 @@ def test_status_code(resp):
 
 
 def test_title(resp):
-    assert_contains(resp, '<title>Python pro</title>')
+    assert_contains(resp, '<title>Python pro - Home</title>')
 
 
 def test_home_link(resp):
