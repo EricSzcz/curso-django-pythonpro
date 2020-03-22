2d75de23026e379376fae5c878c40acd39d1e1c5
Ericszcz <e.ericszcz@gmail.com>
2020-03-22 18:01:14 -0300

    Inclusão de links de aula
    close #122

--- pypro/modulos/urls.py
+++ pypro/modulos/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from pypro.aperitivos.views import video, indice
 from pypro.modulos import views
 
 app_name = 'modulos'
 urlpatterns = [
     path('<slug:slug>', views.detalhe, name='detalhe'),
+    path('aulas/<slug:slug>', views.aula, name='aula'),
 ]
